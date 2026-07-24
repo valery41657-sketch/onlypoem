a4b8aad866a41631fc847a6fd65b7b7bea6b3c29
valery41657-sketch <valery.41657@gmail.com>
2026-07-23 19:30:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// Mensaje de bienvenida
+console.log("Bienvenida a OnlyPoem 💜");
 
+// Animación suave al hacer clic en el botón
+const boton = document.querySelector(".boton");
+
+if (boton) {
+    boton.addEventListener("click", function () {
+        boton.textContent = "💜 Gracias por confiar en OnlyPoem";
+    });
+}
